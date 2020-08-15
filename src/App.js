6f2650bc00853fabcc4a2fbc9b105edb0954f377
Rahul <rahulramakris@gmail.com>
2020-08-15 16:52:45 +0530

    struc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import './App.css';
-import SignIn from './signin'
-import Navbar from './navbar'
+import React from "react";
+import "./App.css";
+import SignIn from "./Components/signin.jsx";
+import Navbar from "./Common/navbar";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper } from "@material-ui/core";
+import SignUp from "./Components/signup";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  gridStyles: {
+    margin: 0,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-      <div>
-          <Navbar />
-          <SignIn />
-      </div>
+    // <div>
+    //   <Router>
+    //     <div>
+    //       <Switch>
+    //         <Route exact path="/">
+    //           <SignIn />
+    //         </Route>
+    //         <Route exact path="/SignUp">
+    //           <SignUp />
+    //         </Route>
+    //       </Switch>
+    //     </div>
+    //   </Router>
+    <Paper style={{ height: "100vh" }}>
+      <Grid Container className={classes.gridStyles}>
+      <Navbar />
+      <SignIn />
+      {/* <SignUp /> */}
+      </Grid>
+    </Paper>
+    // </div>
   );
 }
 
--- src/Common/navbar.js
+++ src/Common/navbar.js
@@ -4,12 +4,16 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+// import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  navBar: {
+    // background: "#424242",
+    background: "#263238",
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -22,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Navbar() {
   const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.navBar}>
       <Toolbar>
         {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
               <MenuIcon />
